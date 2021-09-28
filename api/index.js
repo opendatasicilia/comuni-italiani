247b82bc7e4e0c6f3a8fc46affb2a8f6cc3265ee
Gabriele Scalici <gaba41@gmail.com>
2021-09-28 05:54:27 +0200

    data join
    aggiunge popolazioni

--- api/index.js
+++ api/index.js
@@ -1,20 +1,29 @@
 const express = require('express');
-const csv=require('csvtojson')
+const csv = require('csvtojson')
 const cors = require('cors');
+const merge = require('lodash/merge');
 
 const app = express();
 const port = 8888;
 
 const main = '../dati/main.csv'
+const pop = '../dati/popolazione_2021.csv'
 
 const getData = (f) => {
     let file = csv().fromFile(f)
     return file
 }
 
+const getAll = async() => {
+    const m = await getData(main)
+    const p = await getData(pop)
+    let final = merge(m, p)
+    return final
+}
+
 const getComune = async(comune) =>{
-    const data = await getData(main)
-    let place = data.filter(x => x.comune_codice_istat === comune)
+    const data = await getAll()
+    let place = data.filter(x => x.pro_com_t === comune)
     return place[0]
 }
 
@@ -28,7 +37,7 @@ app.get('/', (req, res) => {
 // Tutti i comuni
 app.get('/comuni', async (req, res) => {
     try {
-        const data = await getData(main)
+        const data = await getAll()
         res.send(data)
     }
     catch(e){
